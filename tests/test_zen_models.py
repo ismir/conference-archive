791cef79468f67c361fea2ae30d3c863ecab6dc1
Eric Humphrey <ejhumphrey@spotify.com>
2018-11-20 21:10:10 -0500

    placeholders for zen.models tests

---
+++ tests/test_zen_models.py
@@ -0,0 +1,39 @@
+import pytest
+
+import zen.models
+
+
+def test_Record():
+    pass
+
+
+def test_Record_dropna():
+    pass
+
+
+def test_DBLP():
+    pass
+
+
+def test_Zenodo():
+    pass
+
+
+def test_IsmirPaper():
+    pass
+
+
+def test_IsmirConference():
+    pass
+
+
+def test_merge():
+    pass
+
+
+def test_creators_to_author():
+    pass
+
+
+def test_author_to_creators():
+    pass
