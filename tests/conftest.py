d56316fc7f5b3a295b664d6d2d42daa21eb79ff3
Eric Humphrey <ejhumphrey@spotify.com>
2018-11-26 08:44:14 -0500

    Working through remaining tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,3 +16,8 @@ def resources_dir():
 @pytest.fixture()
 def scripts_dir(root_dir):
     return os.path.join(root_dir, 'scripts')
+
+
+@pytest.fixture()
+def pdf_file(resources_dir):
+    return os.path.join(resources_dir, 'sample.pdf')
